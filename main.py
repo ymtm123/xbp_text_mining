ce8e21241993e810c324effd83ecdd25f8939b01
ymtm123 <yamazaki@kanagawa-u.ac.jp>
2023-11-21 12:31:24 +0900

    小さな修正

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ importlib.reload(text_mining)
 # 除外する単語の指定
 stopwords = ["する", "ある", "あと", "いる", "いう", "なる", "思う", "言う", "できる"]
 # 除外する頻出上位の指定
-stop_n_top = 3
+stop_n_top = 5
 # 除外する頻出回数以下の指定
-stop_min_freq = 5
+stop_min_freq = 3
 # 除外するノードのエッジ数の指定
-min_edge_frequency = 4
+min_edge_frequency = 3
 
 text_mining.create(stopwords, stop_n_top, stop_min_freq, min_edge_frequency)
 
